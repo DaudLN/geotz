c48b6bf7ec4e81ff634ad74c01c0127cdad0d49d
Daud L Namayala <daudnamayala@gmail.com>
2023-08-02 12:45:14 +0300

    Ship static file

--- tzgeo/settings.py
+++ tzgeo/settings.py
@@ -103,6 +103,13 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "staticfile"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
